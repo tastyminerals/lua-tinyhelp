03f8d74428079c107486bb69423697299459eda3
tastyminerals <tastyminerals@gmail.com>
2016-12-08 12:58:30 +0100

    added 51, 53 xmled docs, fixed some bugs, added rockspec

---
+++ luahelp-1.0-1.rockspec
@@ -0,0 +1,25 @@
+package = "luahelp"
+version = "1.0-1"
+source = {
+   url = "https://github.com/tastyminerals/luahelp",
+   tag = "v1.0"
+}
+description = {
+   summary = "Simple Lua docs xml reader.",
+   detailed = [[
+      Lua docs help system implementation. 
+      Similar to help system used in Python interpreter.
+   ]],
+   homepage = "https://github.com/tastyminerals/luahelp",
+   license = "MIT"
+}
+dependencies = {
+   "lua >= 5.1, < 5.4",
+   "penlight >= 0.8-1",
+   "luafilesystem >=1.6.0-1"
+}
+build = {
+   type = "builtin",
+   luahelp = "src/luahelp.lua",
+   copy_directories = {"docs"}
+}
--- luahelp.lua
+++ luahelp.lua
@@ -1,21 +1,28 @@
 #!/usr/bin/env lua
--- Lua manual xml reader.
+-- Lua docs xml reader.
 
 local luahelp = {}
 
 require "pl"
 
 
--- identify user lua version
-if _VERSION == "Lua 5.3" then
-    fname = "lua_53.xml"
-else if _VERSION == "Lua 5.2" then
-    fname = "lua_52.xml"
-else if _VERSION == "Lua 5.1" then
-    fname = "lua_51.xml"
+function _clean(text)
+    -- replace xml special chars
+    text = string.gsub(text, "&lt;", '<')
+    text = string.gsub(text, "&gt;", '>')
+    text = string.gsub(text, "&amp;", '&')
+    return text
 end
 
-function _read(fname)
+function _read()
+    -- identify user lua version
+    if _VERSION == "Lua 5.3" then
+        fname = path.join("docs","lua_53.xml")
+    elseif _VERSION == "Lua 5.2" then
+        fname = path.join("docs","lua_52.xml")
+    elseif _VERSION == "Lua 5.1" then
+        fname = path.join("docs","lua_51.xml")
+    end
     local fhandle = io.open(fname, 'r')
     local data = fhandle:read("*a")
     fhandle:close()
@@ -23,27 +30,32 @@ function _read(fname)
 end
 
 function luahelp.help(query)
-    root = xml.parse(_read(fname))
+    root = xml.parse(_read())
     -- handle about 
     if query == "about" then print(root[1][1]) return end
-    
+    found = false 
     -- search for query matches
     for item in root[2]:childtags() do
         -- search chapter match
         title = item.attr["title"]
         if (title:match(query)) then
             print(title)
-            print(item[1])
+            print(_clean(item[1]))
+            found = true
         end
         -- search function match
         for initem in item:childtags() do
             name = initem.attr["name"]
             if (name:find(query) == 1) then
                 print(name)
-                print(initem[1])
+                print(_clean(initem[1]))
+                found = true
             end
         end
     end
+    if not found then print("not found") end
 end
+
+-- luahelp.help(arg[1])
 
 return luahelp
